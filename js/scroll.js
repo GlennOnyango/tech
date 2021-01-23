4c339dd2bda1013e31f10c76fc3b9e426f90ae97
Glenn <gonyango@>
2021-01-23 17:59:33 +0300

    index

--- js/scroll.js
+++ js/scroll.js
@@ -4,8 +4,13 @@ $( document ).ready(function() {
     //navbar effect
     if(window.scrollY > 0){
       $( "#mynav" ).addClass("fixed-top");
+
+      $( "#mynav" ).css("background-color", "rgba(0, 0,0, 0.9)");
+      
     }else{
       $( "#mynav" ).removeClass("fixed-top");
+
+      $( "#mynav" ).css("background-color", "rgba(0, 0,0, 0.3)");
     }
 
 
