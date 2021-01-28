19fcaf5915a5256004d520867c016b3a76ab4198
Glenn <gonyango@>
2021-01-28 15:08:42 +0300

    Support plans page

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,61 @@
 $( document ).ready(function() {
 
+  $(".enta-activate").click(()=>{
+
+
+    $(".main-table").toggle( "slide" );
+    
+    $(".main-title").html(`Welcome to Enterprise plan .
+    <small class="text-muted">Downgrade or upgrade at any time.</small>`);
+$(".enterprise").removeClass("d-none");
+$(".p-btn").removeClass("d-none");
+
+});
+
+
+  $(".busi-activate").click(()=>{
+
+
+    $(".main-table").toggle( "slide" );
+    
+    $(".main-title").html(`Welcome to Business plan .
+    <small class="text-muted">Downgrade or upgrade at any time.</small>`);
+$(".business").removeClass("d-none");
+$(".p-btn").removeClass("d-none");
+
+});
+
+
+
+  $(".indv-activate").click(()=>{
+
+
+    $(".main-table").toggle( "slide" );
+    
+    $(".main-title").html(`Welcome to Individual plan .
+    <small class="text-muted">Downgrade or upgrade at any time.</small>`);
+$(".individual").removeClass("d-none");
+$(".p-btn").removeClass("d-none");
+$("footer").toggleClass("fixed-bottom");
+  });
+
+
+  $(".p-btn").click(()=>{
+
+
+    $(".main-table").toggle( "slide" );
+    
+    $(".main-title").html(`Choose the plan that’s right for you.
+    <small class="text-muted">Downgrade or upgrade at any time.</small>`);
+$(".enterprise").addClass("d-none");
+$(".business").addClass("d-none");
+$(".individual").addClass("d-none");
+
+$(".p-btn").addClass("d-none");
+$("footer").removeClass("fixed-bottom");
+
+});
+
   $( window ).scroll(function() {
     //navbar effect
     if(window.scrollY > 0){
